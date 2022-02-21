a9eb2cb2f74be3b5cd066d0831aa415d80149bfe
dezzw <desmond.pc.w@gmail.com>
2022-02-20 22:09:04 -0500

    update nixos layout

--- system/nixos/configuration.nix
+++ system/nixos/configuration.nix
@@ -69,6 +69,7 @@ in
 
   # Configure keymap in X11
   services.xserver.layout = "us";
+  services.xserver.xkbVariant = "dvorak";
   # services.xserver.xkbOptions = "eurosign:e";
 
   # Enable CUPS to print documents.
