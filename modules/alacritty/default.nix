c5a4efd3fcfbf180f2767b3860d0878dccf3024a
dezzw <dw@dezzw.com>
2024-09-25 21:23:00 -0400

    update

---
+++ modules/alacritty/default.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }:
+
+{
+    programs.alacritty = {
+    enable = true;
+    package =
+      pkgs.alacritty; # switching to unstable so i get 0.11 with undercurl support
+    settings = {
+      window = {
+        decorations = "Buttonless";
+        dynamic_title = true;
+        opacity = 0.7;
+        blur = true;
+	option_as_alt = "Both";
+      };
+      scrolling.history = 3000;
+      font = {
+	normal = {
+	  family = "FiraCode Nerd Font Mono";
+	  style = "Regular";
+	};
+	bold.style = "Bold";
+	italic.style = "Italic";
+	bold_italic.style = "Bold Italic";
+	size = if pkgs.stdenvNoCC.isDarwin then 14 else 9;
+      };
+      shell.program = "${pkgs.zsh}/bin/zsh";
+      live_config_reload = true;
+      cursor.vi_mode_style = "Underline";
+      colors.draw_bold_text_with_bright_colors = true;
+      keyboard.bindings = [
+      ];
+    };
+  };
+}
--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -45,9 +45,11 @@ let
 
       comma
 
+      cachix
+
       # misc
       neofetch # display key software/version info in term
-    ] ++ lib.optionals pkgs.stdenv.isDarwin [ yabai ];
+    ] ++ lib.optionals pkgs.stdenv.isDarwin [  ];
 
   guiPkgs = with pkgs;
     [ ] ++ lib.optionals pkgs.stdenv.isDarwin
@@ -71,6 +73,7 @@ in {
               ../emacs
               ../neovim
               ../helix
+              # ../alacritty
             ];
 
   home.sessionVariables = {
@@ -308,45 +311,4 @@ in {
     ];
   };
 
-  programs.zellij = {
-    enable = true;
-    enableZshIntegration = false;
-    settings = {
-      # simplified_ui = true;
-      pane_frames = true; # pane 之间的边框
-      theme = "nord";
-    };
-  };
-    
-  programs.alacritty = {
-    enable = true;
-    package =
-      pkgs.alacritty; # switching to unstable so i get 0.11 with undercurl support
-    settings = {
-      window = {
-        decorations = "Buttonless";
-        dynamic_title = true;
-        opacity = 0.7;
-        blur = true;
-	option_as_alt = "Both";
-      };
-      scrolling.history = 3000;
-      font = {
-	normal = {
-	  family = "FiraCode Nerd Font Mono";
-	  style = "Regular";
-	};
-	bold.style = "Bold";
-	italic.style = "Italic";
-	bold_italic.style = "Bold Italic";
-	size = if pkgs.stdenvNoCC.isDarwin then 14 else 9;
-      };
-      shell.program = "${pkgs.zsh}/bin/zsh";
-      live_config_reload = true;
-      cursor.vi_mode_style = "Underline";
-      colors.draw_bold_text_with_bright_colors = true;
-      keyboard.bindings = [
-      ];
-    };
-  };
 }
